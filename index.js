0953c05f0e05a13155e41aa54d58af5c944b8681
ellenupark <ellenupark@gmail.com>
2020-11-25 20:31:14 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (record) => {
+  let winYear = record.find(function(obj) {
+    return obj["result"] === "W";
+  })
+
+  if (winYear) {
+    return winYear["year"];
+  } else {
+    return undefined;
+  }
+}; 
